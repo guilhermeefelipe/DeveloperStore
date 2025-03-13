ea8a9d59a656decfa7538973d690047e1635087e
guilherme.atanazio <guilherme.atanazio>
2025-03-13 09:20:38 -0300

    Added EncryptionTest

---
+++ tests/DeveloperStore.Tests/UnitTest/EncryptionTests.cs
@@ -0,0 +1,57 @@
+﻿using DeveloperStore.Services;
+using System.Text;
+
+namespace DeveloperStore.Tests.UnitTest;
+
+public class EncryptionTests
+{
+
+    [Fact]
+    public void Test_EncryptDecryptText()
+    {
+        var text = "text text text text";
+
+        var cryptedText = CryptoHelper.Encrypt(text);
+
+        var decryptedText = CryptoHelper.Decrypt(cryptedText);
+
+        Assert.Equal(text, decryptedText);
+    }
+
+    [Fact]
+    public void Test_EncryptDecryptTextAndVerifyFileIntegrity()
+    {
+        var text = new StringBuilder();
+
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+        text.AppendLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.");
+
+        string sourcePath = Path.GetTempFileName();
+        File.WriteAllText(sourcePath, text.ToString());
+
+        string encryptedPath = Path.GetTempFileName();
+        var encryptedContent = CryptoHelper.Encrypt(text.ToString());
+        File.WriteAllText(encryptedPath, encryptedContent);
+
+        Assert.NotEqual(File.ReadAllBytes(sourcePath).Length, File.ReadAllBytes(encryptedPath).Length);
+
+        string decryptedPath = Path.GetTempFileName();
+        string fileContent = File.ReadAllText(encryptedPath);
+        var decryptedContent = CryptoHelper.Decrypt(fileContent);
+        File.WriteAllText(decryptedPath, decryptedContent);
+
+        Assert.Equal(File.ReadAllText(sourcePath), File.ReadAllText(decryptedPath));
+
+        File.Delete(sourcePath);
+        File.Delete(encryptedPath);
+        File.Delete(decryptedPath);
+    }
+
+}
